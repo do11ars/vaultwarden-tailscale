09c727fdba5a96c30822ee8125a7a7f89d2fb473
do11ars <169393033+do11ars@users.noreply.github.com>
2026-07-13 11:13:33 +0700

    Change ALL_PROXY to use socks5h
    Updated the ALL_PROXY environment variable to use 'socks5h' instead of 'socks5'.

--- run-tailscale.sh
+++ run-tailscale.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-/render/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 --disable-peer-api &
+/render/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
 PID=$!
 
 until /render/tailscale up --authkey="${TAILSCALE_AUTHKEY}" --hostname="${RENDER_SERVICE_NAME}"; do
   sleep 0.1
 done
-export ALL_PROXY=socks5://localhost:1055/
+export ALL_PROXY=socks5h://localhost:1055/
 tailscale_ip=$(/render/tailscale ip)
 echo "Tailscale is up at IP ${tailscale_ip}"
 
