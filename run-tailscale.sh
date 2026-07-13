b6bbcff4d389c609bc2b3dc28331e0222a720643
do11ars <169393033+do11ars@users.noreply.github.com>
2026-07-13 09:40:46 +0700

    Remove advertise routes from Tailscale startup

---
+++ run-tailscale.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+/render/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
+PID=$!
+
+until /render/tailscale up --authkey="${TAILSCALE_AUTHKEY}" --hostname="${RENDER_SERVICE_NAME}"; do
+  sleep 0.1
+done
+export ALL_PROXY=socks5://localhost:1055/
+tailscale_ip=$(/render/tailscale ip)
+echo "Tailscale is up at IP ${tailscale_ip}"
+/render/vaultwarden &
+VAULT_PID=$!
+
+wait ${PID} ${VAULT_PID}
