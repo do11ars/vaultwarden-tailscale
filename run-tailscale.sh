91387d267d87ec25d7cf7bcea12954a3bae51e23
do11ars <169393033+do11ars@users.noreply.github.com>
2026-07-13 10:47:41 +0700

    Add proxy settings for vaultwarden service

--- run-tailscale.sh
+++ run-tailscale.sh
@@ -9,6 +9,8 @@ done
 
 tailscale_ip=$(/render/tailscale ip)
 echo "Tailscale is up at IP ${tailscale_ip}"
+
+cd /
 ALL_PROXY=socks5://localhost:1055/ HTTP_PROXY=http://localhost:1055/ http_proxy=http://localhost:1055/ /render/vaultwarden &
 VAULT_PID=$!
 
